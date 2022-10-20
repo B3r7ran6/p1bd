c37ba8ffd2c7b090502f70f5687ab58527e4e415
Bertrand <b.devantoy@gmail.com>
2022-10-20 12:45:57 +0200

    fix header

--- main.js
+++
@@ -1,9 +0,0 @@
-
-function openCloseNav(x) {
-    x.classList.toggle("change");
-    if (document.querySelectorAll('nav')[0].style.width != 0) {
-        document.querySelectorAll('nav')[0].style.width = "0";
-    } else {
-        document.querySelectorAll('nav')[0].style.width = "initial";
-    }
-}
